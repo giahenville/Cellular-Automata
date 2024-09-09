09cbb180abf0333fc82defb4132608178576265c
Gia Henville <140769584+giabui@users.noreply.github.com>
2024-09-09 13:50:04 -0400

    feat: Create next generation cells and don't erase background of previous generation

--- sketch.js
+++ sketch.js
@@ -1,31 +1,41 @@
-const cells = [1, 0, 0, 1, 0, 1, 0, 1, 1, 0];
-let w = 40;
-
+let cells = [];
+let w = 10;
+let y = 0;
 function setup() {
   createCanvas(400, 400);
+  let total = width / w;
+  // randomly select initial state of cells
+  for (let i = 0; i < total; i++) {
+    cells[i] = floor(random(2));
+  }
+  background(255);
 }
 
 function draw() {
-  background(255);
+  
   for (let i = 0; i < cells.length; i++) {
     let x = i * w;
     stroke(0);
     fill(255 - cells[i] * 255);
-    square(x, 0, w)
+    square(x, y, w)
   }
-
+  y += w;
   // generate next gen
   let nextGen = [];
+  // hard code edges state 
+  nextGen[0] = cells[0];
+  nextGen[cells.length-1] = cells[cells.length - 1];
+
   for (let i = 1; i < cells.length - 1; i++) {
     let left = cells[i - 1];
     let middle = cells[i];
     let right = cells[i + 1];
 
     let newState = calculateState(left, middle, right);
-    nextGen.push(newCell);
+    nextGen[i] = newState;
   }
   cells = nextGen;
-};
+}
 
 // calculate new cell state based on arbitrary rules
 function calculateState(a, b, c) {
